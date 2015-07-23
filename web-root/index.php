035acc1237b0c80d18985801fde5b06a5e1feacb
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2015-07-23 15:11:18 -0400

    fix follow button in layout

--- web-root/index.php
+++ web-root/index.php
@@ -152,7 +152,8 @@ header('Content-Type: text/html; charset=utf-8');
         <div class="sat-info-value" id="sat-period">100</div> min
       </div>
       <div class="sat-info-row">
-        Follow <input type="checkbox" name="sat-follow" onclick="checkFollow(this)" />
+        <div class="sat-info-key">Follow</div>
+        <input type="checkbox" name="sat-follow" onclick="checkFollow(this)" />
       </div>
     </div>
     <div id="zoom-controls">
