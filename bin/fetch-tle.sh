2d0dce1f1e224e276a01503469ffbfa32a8c13f5
Andre-John Mas <andrejohn.mas@gmail.com>
2023-11-25 14:59:30 -0500

    improved download script and updated TLE data

--- bin/fetch-tle.sh
+++ bin/fetch-tle.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
-tle_file="../public/data/TLE.json"
-## curl > public/data/TLE.json
+basepath=`dirname "$0"`
+cd "$basepath"
 
+identity=$SPACETRACK_IDENTITY
+password=$SPACETRACK_PASSWORD
+
+if [ -z "$SPACETRACK_IDENTITY" ] || [ -z "$SPACETRACK_PASSWORD" ]
+then
+  echo 'ensure both "SPACETRACK_IDENTITY" and "SPACETRACK_PASSWORD" are set'
+fi
+
+source_url="https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/EPOCH/%3Enow-30/orderby/NORAD_CAT_ID/format/json"
+tle_file="../public/data/TLE.json"
 output_file="../public/data/attributed-TLE.json"
 current_date=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+cookie_jar="/tmp/$USER-cookiejar"
+
+echo "Downloading TLE data"
+curl -c $cookie_jar -b $cookie_jar https://www.space-track.org/ajaxauth/login \
+   -d "identity=$identity&password=$password"
+
+curl ---limit-rate 100K -cookie $cookie_jar "${source_url}" > $tle_file
+
+echo "Generating Attributed TLE file"
 echo "{
   \"source\": {
     \"name\": \"Space-Track.org\",
@@ -16,4 +35,3 @@ echo "{
 
 cat $tle_file >> $output_file
 echo "\n}\n" >> $output_file
-
